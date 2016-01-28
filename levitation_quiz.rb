826936b7ca511de1656aeb8b1fee15292f748284
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-01-28 16:10:09 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,19 @@
-
 def levitation_quiz
-	#your code here
+	loop do
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+  end
+  puts "You passed the quiz!"
 end
 
+#def annoying
+    #loop do
+        #puts "What's up, Doc?"
+        #answer = gets.chomp
+        #break if answer == "STOP"
+    #end
+    #puts "Okay, okay, jeez. I'll stop. Sorry."
+#end
 
+#annoying
